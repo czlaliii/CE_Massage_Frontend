04889fafd6db205e7e92299ba231e40709bee335
Lajos Czinege <czinegelali03@gmail.com>
2026-02-05 21:43:08 +0100

    Navigation and logo added to footer

--- CE_Massage_Frontend/src/app/footer/footer.ts
+++ CE_Massage_Frontend/src/app/footer/footer.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
