4e0ab194fb80e59e9098287f353add24689ab0d0
Lajos Czinege <czinegelali03@gmail.com>
2026-02-11 14:14:55 +0100

    - Gallery hero has been replaced - Gallery structure changed to grid - Made the footer and gallery page responsive

--- CE_Massage_Frontend/src/app/pages/gallery/gallery.ts
+++ CE_Massage_Frontend/src/app/pages/gallery/gallery.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [MatGridListModule, NgOptimizedImage],
+  imports: [NgOptimizedImage],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
 export class GalleryComponent {
-
 }
