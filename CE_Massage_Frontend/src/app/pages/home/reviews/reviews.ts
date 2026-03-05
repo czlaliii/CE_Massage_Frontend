5709a3b70cb5afa219000d55471a9058bbcf739c
Lajos Czinege <czinegelali03@gmail.com>
2026-03-05 10:56:31 +0100

    Reviews section style updated

--- CE_Massage_Frontend/src/app/pages/home/reviews/reviews.ts
+++ CE_Massage_Frontend/src/app/pages/home/reviews/reviews.ts
@@ -8,12 +8,11 @@ import { ReviewsService } from './reviews.service';
   templateUrl: './reviews.html',
   styleUrl: './reviews.css',
 })
-
 export class Reviews {
 
   currentIndex = 0;
   autoPlayInterval: any;
-  cardsToShow = 3;
+  readonly cardsToShow = 1;
 
   constructor(private reviewsService: ReviewsService) {}
 
@@ -22,74 +21,45 @@ export class Reviews {
   }
 
   ngOnInit() {
-    this.updateCardsToShow();
-    window.addEventListener('resize', () => this.updateCardsToShow());
     this.startAutoPlay();
   }
 
   ngOnDestroy() {
     this.stopAutoPlay();
-    window.removeEventListener('resize', () => this.updateCardsToShow());
-  }
-
-  updateCardsToShow() {
-    if (window.innerWidth <= 768) {
-      this.cardsToShow = 1;
-    } else if (window.innerWidth <= 1024) {
-      this.cardsToShow = 2;
-    } else {
-      this.cardsToShow = 3;
-    }
   }
 
   get maxIndex(): number {
-    return Math.max(0, this.reviews.length - this.cardsToShow);
+    return Math.max(0, this.reviews.length - 1);
   }
 
   get transformValue(): string {
-    const cardWidth = 100 / this.cardsToShow;
-    return `translateX(-${this.currentIndex * cardWidth}%)`;
+    return `translateX(-${this.currentIndex * 100}%)`;
   }
 
   nextSlide() {
-    if (this.currentIndex < this.maxIndex) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0;
-    }
+    this.currentIndex = this.currentIndex < this.maxIndex
+      ? this.currentIndex + 1
+      : 0;
   }
 
   prevSlide() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.maxIndex;
-    }
+    this.currentIndex = this.currentIndex > 0
+      ? this.currentIndex - 1
+      : this.maxIndex;
   }
 
   goToSlide(index: number) {
-    if (index <= this.maxIndex) {
-      this.currentIndex = index;
-    }
+    this.currentIndex = index;
   }
 
   startAutoPlay() {
-    this.autoPlayInterval = setInterval(() => {
-      this.nextSlide();
-    }, 5000);
+    this.autoPlayInterval = setInterval(() => this.nextSlide(), 5000);
   }
 
   stopAutoPlay() {
-    if (this.autoPlayInterval) {
-      clearInterval(this.autoPlayInterval);
-    }
+    clearInterval(this.autoPlayInterval);
   }
 
-  pauseAutoPlay() {
-    this.stopAutoPlay();
-  }
-
-  resumeAutoPlay() {
-    this.startAutoPlay();
-  }
+  pauseAutoPlay() { this.stopAutoPlay(); }
+  resumeAutoPlay() { this.startAutoPlay(); }
 }
