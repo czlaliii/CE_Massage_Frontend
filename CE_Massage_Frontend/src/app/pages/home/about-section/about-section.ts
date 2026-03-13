160bd0cf9cfcc401c50ae953c38483cc670ddcb0
Lajos Czinege <czinegelali03@gmail.com>
2026-03-13 16:50:48 +0100

    Home page - about section's 'more' button fixed

--- CE_Massage_Frontend/src/app/pages/home/about-section/about-section.ts
+++ CE_Massage_Frontend/src/app/pages/home/about-section/about-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-section',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './about-section.html',
   styleUrl: './about-section.css',
 })
