e046a1a13d6d0302f88a50130b284230478de2d4
Lajos Czinege <czinegelali03@gmail.com>
2026-03-29 18:11:13 +0200

    Faq added to header

--- CE_Massage_Frontend/src/app/pages/faq/faq.ts
+++ CE_Massage_Frontend/src/app/pages/faq/faq.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ExpansionComponent } from './components/expansion/expansion';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-faq',
-  imports: [ExpansionComponent, MatButtonModule],
+  imports: [ExpansionComponent, MatButtonModule, RouterLink],
   templateUrl: './faq.html',
   styleUrl: './faq.css',
 })
