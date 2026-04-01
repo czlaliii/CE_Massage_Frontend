b38f0cbb304b23e13eb5dc11ca11049c9a63b65c
Lajos Czinege <czinegelali03@gmail.com>
2026-04-01 22:28:45 +0200

    Header major update on functions

--- CE_Massage_Frontend/src/app/header/header.ts
+++ CE_Massage_Frontend/src/app/header/header.ts
@@ -1,35 +1,44 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatMenuModule, MatButtonModule, NgOptimizedImage],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.css',
   encapsulation: ViewEncapsulation.None
 })
 export class Header {
   menuOpen = false;
-  servicesOpen = false;
+  serviceOpen = false;
+
+  constructor(private eRef: ElementRef) {}
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
     if (!this.menuOpen) {
-      this.servicesOpen = false;
+      this.serviceOpen = false;
     }
-  }
-
-  toggleServices(event: Event) {
-    event.stopPropagation();
-    this.servicesOpen = !this.servicesOpen;
   }
 
   closeMenu() {
     this.menuOpen = false;
-    this.servicesOpen = false;
+    this.serviceOpen = false;
+  }
+
+  toggleServices() {
+    if (window.innerWidth <= 1024) {
+      this.serviceOpen = !this.serviceOpen;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.menuOpen = false;
+      this.serviceOpen = false;
+    }
   }
 }
 
