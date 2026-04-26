f9baf2501568ee86498fb44220830885ea0f2b73
Lajos Czinege <czinegelali03@gmail.com>
2026-04-26 16:14:05 +0200

    Expanse img responsive fix

--- CE_Massage_Frontend/src/app/pages/services/expanse/expanse.ts
+++ CE_Massage_Frontend/src/app/pages/services/expanse/expanse.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../components/hero/hero';
-import { ScrollAnimateDirective } from '../../../shared/directives/scroll.directive';
 import { NgOptimizedImage } from '@angular/common';
 import { Card } from '../components/card/card';
 import { CardGrid } from '../components/card-grid/card-grid';
-import { AboutComponent } from "../../about/about";
 import { ListItem } from "../components/list-item/list-item";
 import { List } from "../components/list/list";
 
@@ -13,7 +11,7 @@ import { List } from "../components/list/list";
   standalone: true,
   templateUrl: './expanse.html',
   styleUrl: './expanse.css',
-  imports: [Hero, ScrollAnimateDirective, NgOptimizedImage, Card, CardGrid, AboutComponent, ListItem, List]
+  imports: [Hero, NgOptimizedImage, Card, CardGrid, ListItem, List]
 })
 export class ExpanseComponent {
 
