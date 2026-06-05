1ccfb3ae185a22ed74cd0a5c09f37eb0d3405a04
Lajos Czinege <czinegelali03@gmail.com>
2026-06-05 18:37:52 +0200

    i18n added to FAQ

--- CE_Massage_Frontend/src/app/pages/faq/faq.ts
+++ CE_Massage_Frontend/src/app/pages/faq/faq.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ExpansionComponent } from './components/expansion/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { Button } from "../../shared/button/button";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-faq',
-  imports: [ExpansionComponent, MatButtonModule, Button],
+  imports: [ExpansionComponent, MatButtonModule, Button, TranslatePipe],
   templateUrl: './faq.html',
   styleUrl: './faq.css',
 })
