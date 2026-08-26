c4d795100a00eebc05b7d5a4aa00180bf11e21c0
Lajos Czinege <czinegelali03@gmail.com>
2026-08-26 21:21:11 +0200

    Update on the prices list: - Add new prices - New toggle info panels to describe the massages - Add Book an Appointment button to the bottom - Toggle info panel for other treatments - New price note for swedish and premium massages

--- CE_Massage_Frontend/src/app/pages/prices/prices.ts
+++ CE_Massage_Frontend/src/app/pages/prices/prices.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Button } from '../../shared/button/button';
 import { TranslatePipe } from '@ngx-translate/core';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-prices',
@@ -10,4 +11,41 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class PricesComponent {
 
+  openInfo = signal<
+    'five' | 'expanse' | 'vibecodes' | 'swedish' | 'premium' | null
+  >(null);
+
+  openTreatment = signal<string | null>(null);
+
+  toggleInfo(
+    service: 'five' | 'expanse' | 'vibecodes' | 'swedish' | 'premium'
+  ): void {
+
+    if (this.openInfo() === service) {
+
+      this.openInfo.set(null);
+
+    } else {
+
+      this.openInfo.set(service);
+
+    }
+  }
+
+
+  toggleTreatment(
+    treatment: string
+  ): void {
+
+    if (this.openTreatment() === treatment) {
+
+      this.openTreatment.set(null);
+
+    } else {
+
+      this.openTreatment.set(treatment);
+
+    }
+
+  }
 }
